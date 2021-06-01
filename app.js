9479340c39ca85563150eaeee50de6508b986edc
Kathy Tavia <kathy.tavia@gmail.com>
2021-06-01 10:05:21 +0200

    updated app.js and added axios & added content to index view

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.set('view engine', 'pug');
+
+app.get('/', async (req, res) => {
+  const query = await axios.get('http://localhost:3001/results');
+  res.render('index', { employees: query.data });
 });
 
 app.listen(3000, () => {
